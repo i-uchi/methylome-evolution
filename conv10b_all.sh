3e5c8138dc8bb72fe502fcfc4bc307f3755089cf
Ikuo Uchiyama <uchiyama@nibb.ac.jp>
2025-08-12 19:40:48 +0900

    move two commands to bin

--- conv10b_all.sh
+++ conv10b_all.sh
@@ -5,10 +5,15 @@ conv_seqdir=seq10b
 tmp_alidir=tmp_align
 orig_alidir=orig_align
 conv_alidir=ali10b
-cluster_file=core.sample.o0
-#cluster_file=core.all.o0
 
-motlist_file=motlist_0.7.out
+origseq_file=origseq.fna	## Genomic sequence file in FASTA format
+annotation_file=origseq.gff	## Annotation file in GFF format
+gbk_file=origseq.gbk	## GenBank format file (alternative to annotation file in GFF format)
+
+orthogroup_file=core.sample.o0	## Ortholog group file in DomClust "o0" format
+#orthogroup_file=core.all.o0
+
+motlist_file=motlist_0.7.out	## motif list file listing all the motifs identified in either strain.
 
 ## cutoff
 use_motif=1	# use motif to determine methylated bases
@@ -36,13 +41,13 @@ for dir in $indir/*; do
 	if [ "$use_motif" == 1 -a -f $dir/motif.data ]; then
 		## use motif file
 		if [ -f $motlist_file ]; then
-			bin/conv10b.pl -minr=$minr -limit_motif_file=$motlist_file -limit_cut=$limit_cut $dir/motif.data $dir/origseq.fna > $odir/conv10b.fas
+			bin/conv10b.pl -minr=$minr -limit_motif_file=$motlist_file -limit_cut=$limit_cut $dir/motif.data $dir/$origseq_file > $odir/conv10b.fas
 		else
-			bin/conv10b.pl -minr=$minr $dir/motif.data $dir/origseq.fna  > $odir/conv10b.fas
+			bin/conv10b.pl -minr=$minr $dir/motif.data $dir/${origseq_file}  > $odir/conv10b.fas
 		fi
 	elif [ -f $dir/modifications.gff ]; then
 		## use modification file
-		bin/conv10b.pl -modfile -min_score=$min_score $dir/modifications.gff $dir/origseq.fna > $odir/conv10b.fas
+		bin/conv10b.pl -modfile -min_score=$min_score $dir/modifications.gff $dir/${origseq_file} > $odir/conv10b.fas
 	fi
 done
 
@@ -51,8 +56,8 @@ echo "## 2. Extract CDS"
 for str_dir in $indir/*; do
 	str_name=`basename $str_dir`
 	echo $str_name
-	if [ -f $str_dir/origseq.gff ]; then
-		bin/extseq_gff.pl -spname=$str_name $str_dir/origseq.gff $str_dir/origseq.fna > $str_dir/origseq.cds.fna
+	if [ -f $str_dir/${annotation_file} ]; then
+		bin/extseq_gff.pl -spname=$str_name $str_dir/${annotation_file} $str_dir/${origseq_file} > $str_dir/origseq.cds.fna
 		c_str_dir=$conv_seqdir/$str_name
 		for infas in $c_str_dir/conv10b*.fas; do
 			if [[ "$infas" =~ cds ]]; then
@@ -60,10 +65,10 @@ for str_dir in $indir/*; do
 				continue
 			fi
 			outfas=`echo $infas |sed s/.fas/.cds.fas/`
-			bin/extseq_gff.pl -spname=$str_name $str_dir/origseq.gff $infas > $outfas
+			bin/extseq_gff.pl -spname=$str_name $str_dir/${annotation_file} $infas > $outfas
 		done
-	elif [ -f $str_dir/origseq.gbk ]; then
-		bin/extseq_gbk.pl -spname=$str_name $str_dir/origseq.gbk  > $str_dir/origseq.cds.fna
+	elif [ -f $str_dir/${gbk_file} ]; then
+		bin/extseq_gbk.pl -spname=$str_name $str_dir/${gbk_file}  > $str_dir/origseq.cds.fna
 
 		for infas in $c_str_dir/conv10b*.fas; do
 			if [[ "$infas" =~ cds ]]; then
@@ -71,7 +76,7 @@ for str_dir in $indir/*; do
 				continue
 			fi
 			outfas=`echo $infas |sed s/.fas/.cds.fas/`
-			bin/extseq_gbk.pl -spname=$str_name $str_dir/origseq.gbk $infas > $outfas
+			bin/extseq_gbk.pl -spname=$str_name $str_dir/${gbk_file} $infas > $outfas
 		done
 	fi
 done
@@ -80,10 +85,10 @@ cat $indir/*/origseq.cds.fna > $origseq
 cat $conv_seqdir/*/conv10b.cds.fas > $convseq
 
 echo "## 3. Create alignments"
-./extract_core_seq.pl $cluster_file $origseq $tmp_alidir
-./exec_align.sh $tmp_alidir $orig_alidir
+bin/extract_core_seq.pl $orthogroup_file $origseq $tmp_alidir
+bin/exec_align.sh $tmp_alidir $orig_alidir
 
 
 echo "## 4. Create convered alignments"
 
-bin/conv_align.pl $convseq $orig_alidir $conv_alidir $cluster_file
+bin/conv_align.pl $convseq $orig_alidir $conv_alidir $orthogroup_file
